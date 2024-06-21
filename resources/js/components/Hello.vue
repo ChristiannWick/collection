84489edefe94dd7f1987fd62845c9b5b0f6e446a
Remil Christian U. Casilang <smile_zx@yahoo.com>
2024-06-21 13:40:10 +0800

    testing

--- resources/js/components/Hello.vue
+++ resources/js/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
     <v-card>
         <v-card-title style="background-color:#FFCC80">Hello</v-card-title>
+        <v-card-subtitle>testing</v-card-subtitle>
         <v-card-text>
             <p>some transcations & logics here....</p>
         </v-card-text>
